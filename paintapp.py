feec3a8ddab5fbe323f95e0c9634d958450aa7dc
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-06-12 20:12:04 +0100

    Add files via upload

---
+++ paintapp.py
@@ -0,0 +1,35 @@
+import turtle
+import random
+
+wn = turtle.Screen()
+wn.title("Paint Application")
+
+turt = turtle.Turtle()
+
+turt.hideturtle()
+
+print("0 - Red")
+print("1 - Orange")
+print("2 - Yellow")
+print("3 - Green")
+print("4 - Blue")
+print("5 - Black")
+print("6 - Purple")
+
+
+custom = int(input("Choose a color (relative number): "))
+
+colorLibraryRaw = ["red", "orange", "yellow", "green", "blue", "black", "purple"]
+
+colorLibrary = colorLibraryRaw[custom]
+
+turt.color(colorLibrary)
+
+def func(x, y):
+
+    turt.goto(x, y)
+    turt.write(str(x) + ", " + str(y))
+
+wn.onclick(func)
+
+wn.mainloop()
